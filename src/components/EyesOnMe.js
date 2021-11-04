7f8753839f8fd339b66f7c22a5eaee9d398ffb97
mckinseyleckenby <leckenbym11@gmail.com>
2021-11-04 08:50:49 -0500

    Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React from "react";
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function textButton() {
+        console.log("Good!");
+    }
+
+    function textButtonBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+    return (
+        <button onFocus={textButton} onBlur={textButtonBlur}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe
+
